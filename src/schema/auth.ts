ef44d0edf72df71c9a80b6ee5a707d3bd12834ba
mon7792 <monish7792@gmail.com>
2025-07-20 09:08:46 +0100

    feat: + drizzle + pg adapter

---
+++ src/schema/auth.ts
@@ -0,0 +1,14 @@
+/**
+ * THE BELOW FILE SCHEMA IS CREATED MANUALLY with reference from ./better-auth_migrations/*
+ * */
+import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+
+export const user = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  emailVerified: boolean("emailVerified").notNull(),
+  image: text("image"),
+  createdAt: timestamp("createdAt").notNull(),
+  updatedAt: timestamp("updatedAt").notNull(),
+});
